fe5937c2aaf11c8ed2a5f71878154342aa732c3b
Tom Abbott <twabbott@outlook.com>
2017-03-19 23:14:03 -0600

    Added comments to WinFormsException.cs

--- lib/WinForms/FeralNerd.WinForms/WinFormsException.cs
+++ lib/WinForms/FeralNerd.WinForms/WinFormsException.cs
@@ -2,13 +2,40 @@
 
 namespace FeralNerd.WinForms
 {
+    /// <summary>
+    ///     A general-purpose exception class for the WinForms library.
+    /// </summary>
     public class WinFormsException : Exception
     {
+        /// <summary>
+        ///     Creates a new WinFormsException object.
+        /// </summary>
+        /// 
+        /// <param name="format">
+        ///     The exception message.
+        /// </param>
+        /// <param name="args">
+        ///     Variable-argument list.
+        /// </param>
         public WinFormsException(string format, params object[] args)
             : base(string.Format(format, args))
         {
         }
 
+
+        /// <summary>
+        ///     Creates a new WinFormsException object.
+        /// </summary>
+        /// 
+        /// <param name="innerException">
+        ///     An inner exception.
+        /// </param>
+        /// <param name="format">
+        ///     The exception message.
+        /// </param>
+        /// <param name="args">
+        ///     Variable-argument list.
+        /// </param>
         public WinFormsException(Exception innerException, string format, params object[] args)
             : base(string.Format(format, args), innerException)
         {
